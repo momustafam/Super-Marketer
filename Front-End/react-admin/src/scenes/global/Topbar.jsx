603c6fa12e0fb05b99cce37c08971f25e1fda70b
yahia <yahia2305618@miuegypt.edu.eg>
2025-07-23 15:36:31 +0300

    Add trending icons and update dashboard metrics for better visualization

--- Front-End/react-admin/src/scenes/global/Topbar.jsx
+++ Front-End/react-admin/src/scenes/global/Topbar.jsx
@@ -7,7 +7,6 @@ import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import SearchIcon from "@mui/icons-material/Search";
 
 const Topbar = () => {
     const theme = useTheme();
@@ -15,19 +14,12 @@ const Topbar = () => {
     const colorMode = useContext(ColorModeContext);
 
     return (
-        <Box display="flex" justifyContent="space-between" p={2}>
-            {/* Search Bar */}
-            <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px">
-                <InputBase sx={{ ml: 2, flex: 1}} placeholder="Search" />
-                <IconButton type="button" sx={{ p: 1}}>
-                    <SearchIcon />
-                </IconButton>
-            </Box>
+        <Box display="flex" p={2}>
 
             {/* ICONS */}
-            <Box display="flex">
+            <Box display="flex" justifyContent="flex-end" alignItems="center">
                 <IconButton onClick={colorMode.toggleColorMode}>
-                    {theme.palette.mode === "dark" ? (
+                    {theme.palette.mode === "light" ? (
                         <DarkModeOutlinedIcon />
                     ) : (
                         <LightModeOutlinedIcon />
