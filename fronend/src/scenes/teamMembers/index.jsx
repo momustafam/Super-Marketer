55e2b6e6d7c9a47f9f76b1cfc144fb84940dcbf4
Mohammed Mustafa <mohammedmustafa112025@gmail.com>
2025-07-25 20:06:47 +0300

    upload transactions data

---
+++ fronend/src/scenes/teamMembers/index.jsx
@@ -0,0 +1,127 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import { mockDataTeam } from "../../data/mockData";
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
+import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
+import Header from "../../components/Header";
+
+
+const TeamMembers = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode)
+    const columns = [
+        {
+            field: "id", 
+            headerName: "ID",
+            headerAlign: "center",
+            align: "center",
+        },
+        {
+            field: "name",
+            headerName: "Name",
+            flex: 1,
+            cellClassName: "name-column--cell",
+            headerAlign: "center",
+            align: "center",
+        },
+        {
+            field: "age",
+            headerName: "Age",
+            type: "number",
+            headerAlign: "center",
+            align: "center",
+        },
+        {
+            field: "phone",
+            headerName: "Phone Number",
+            flex: 1,
+            headerAlign: "center",
+            align: "center",
+        },
+        {
+            field: "email",
+            headerName: "Email",
+            flex: 1,
+            headerAlign: "center",
+            align: "center",
+        },
+        {
+            field: "accessLevel",
+            headerName: "Access Level",
+            flex: 1,
+            headerAlign: "center",
+            align: "center",
+            renderCell: ({ row: { access }}) => {
+                return (
+                    <Box
+                        width="60%"
+                        m="0 auto"
+                        mt="10px"
+                        p="5px"
+                        display="flex"
+                        justifyContent="center"
+                        backgroundColor={
+                        access === "admin"
+                            ? colors.greenAccent[600]
+                            : access === "manager"
+                            ? colors.greenAccent[700]
+                            : colors.greenAccent[700]
+                        }
+                        borderRadius="4px"
+                    >
+                        {access === "admin" && <AdminPanelSettingsOutlinedIcon />}
+                        {access === "manager" && <SecurityOutlinedIcon />}
+                        {access === "user" && <LockOpenOutlinedIcon />}
+                        <Typography color={colors.gray[100]} sx={{ ml: "5px" }}>
+                            {access}
+                        </Typography>
+                    </Box>
+                );
+            },
+        },
+    ];
+
+    return (
+        <Box m="20px">
+            <Header title="Manage Team" subtitle="Manage roles and permissions for team members" />
+            <Box
+                m="40px 0 0 0"
+                height="75vh"
+                sx={{
+                    "& .MuiDataGrid-root": {
+                        border: "none",
+                    },
+                    "& .MuiDataGrid-cell": {
+                        borderBottom: "none",
+                    },
+                    "& .name-column--cell": {
+                        color: colors.greenAccent[300],
+                    },
+                    "& .MuiDataGrid-columnHeaders": {
+                        backgroundColor: colors.blueAccent[700],
+                        borderBottom: "none",
+                    },
+                    "& .MuiDataGrid-virtualScroller": {
+                        backgroundColor: colors.primary[400],
+                    },
+                    "& .MuiDataGrid-footerContainer": {
+                        borderTop: "none",
+                        backgroundColor: colors.blueAccent[700],
+                    },
+                    "& .MuiCheckbox-root": {
+                        color: `${colors.greenAccent[200]} !important`,
+                    },
+                }}
+            >
+                <DataGrid
+                    rows={mockDataTeam}
+                    columns={columns}
+                />
+            </Box>
+        </Box>
+    );
+};
+
+export default TeamMembers;
